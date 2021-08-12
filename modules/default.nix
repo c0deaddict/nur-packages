41d3b9f5bbafd2e3e26ed3af22c273d75fd901e6
Jos van Bakel <jos@codeaddict.org>
2021-08-12 09:46:14 +0200

    modules.prometheus-smartmon-collector: init

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,7 @@
 {
   prometheus-unbound-exporter = ./prometheus-unbound-exporter;
   prometheus-nats-exporter = ./prometheus-nats-exporter;
+  prometheus-smartmon-collector = ./prometheus-smartmon-collector;
   import-garmin-connect = ./import-garmin-connect;
   solaredge-influx = ./solaredge-influx;
   apt-cacher-ng = ./apt-cacher-ng;
