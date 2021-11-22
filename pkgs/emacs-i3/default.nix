53fbf99983d25a3a2026c510f64559f6ff259e80
Jos van Bakel <jos@codeaddict.org>
2021-11-22 11:23:40 +0100

    emacs-i3: 0.1.2 -> 0.1.3

--- pkgs/emacs-i3/default.nix
+++ pkgs/emacs-i3/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "emacs-i3";
-  version = "0.1.2";
+  version = "0.1.3";
 
   src = fetchFromGitHub {
     owner = "c0deaddict";
     repo = pname;
     rev = version;
-    sha256 = "sha256-AfEP2QZfBug84qnLpVZTPsFedMdcgSt++KQSvpOhORc=";
+    sha256 = "sha256-MGLxQEObD2l3zIF2Kv+PJhHuLJKMblW4dRW1QNL4iJk=";
   };
 
-  cargoSha256 = "sha256-NR6JHpbPmRr6VbkiWd9DwaNMEjNAJ48qXTxOmJxQAyM=";
+  cargoSha256 = "sha256-x4dy7lEBov+/7nn7oTqQta2l6gFb5FnWEA3Ws2XPXpQ=";
 
   meta = with lib; {
     description = "Emacs i3 unified window management";
