a7925246dd6ddf3fd91650a3bc609b9817b28b93
Jos van Bakel <jos@codeaddict.org>
2023-12-01 13:14:07 +0100

    remove old packages and rewrite to new hash

--- modules/default.nix
+++ modules/default.nix
@@ -3,11 +3,7 @@
   acme-dns = ./acme-dns;
   duplicity = ./duplicity;
   etlegacy = ./etlegacy;
-  import-garmin-connect = ./import-garmin-connect;
   prometheus-nats-exporter = ./prometheus-nats-exporter;
   prometheus-nvme-collector = ./prometheus-nvme-collector;
-  prometheus-openweathermap-exporter = ./prometheus-openweathermap-exporter;
   prometheus-smartmon-collector = ./prometheus-smartmon-collector;
-  prometheus-unbound-exporter = ./prometheus-unbound-exporter;
-  solaredge-influx = ./solaredge-influx;
 }
